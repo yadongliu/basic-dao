aac8c44bd090785115ed2e47dbf486ff91cdb798
Yadong Liu <yadong.github@gmail.com>
2022-01-04 11:53:31 -0800

    revoking roles and chain error handling

---
+++ scripts/11-revoke-roles.js
@@ -0,0 +1,33 @@
+import sdk from "./1-initialize-sdk.js";
+
+const tokenModule = sdk.getTokenModule(
+  "0x1228f38315b531e4dB4F8c694766eeB00c2e9723",
+);
+
+(async () => {
+  try {
+    // Log the current roles.
+    console.log(
+      "👀 Roles that exist right now:",
+      await tokenModule.getAllRoleMembers()
+    );
+
+    await tokenModule.revokeRole("transfer", process.env.WALLET_ADDRESS);
+    console.log(
+        "🎉 Roles after revoking ourselves",
+        await tokenModule.getAllRoleMembers()
+      );
+  
+    // Revoke all the superpowers your wallet had over the ERC-20 contract.
+    /*
+    await tokenModule.revokeAllRolesFromAddress(process.env.WALLET_ADDRESS);
+    console.log(
+      "🎉 Roles after revoking ourselves",
+      await tokenModule.getAllRoleMembers()
+    );
+    console.log("✅ Successfully revoked our superpowers from the ERC-20 contract");
+*/
+  } catch (error) {
+    console.error("Failed to revoke ourselves from the DAO treasury", error);
+  }
+})();
--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useEffect, useMemo, useState } from "react";
 import { useWeb3 } from "@3rdweb/hooks";
 import { ThirdwebSDK } from "@3rdweb/sdk";
 import { ethers } from "ethers";
+import { UnsupportedChainIdError } from "@web3-react/core";
 
 const sdk = new ThirdwebSDK("rinkeby");
 const contractAddr = "0x72a8d9178F94dD13eAa73B7fD44334A2a174AdEe";
@@ -189,6 +190,18 @@ const App = () => {
     });
   }
 
+  if (error instanceof UnsupportedChainIdError ) {
+    return (
+      <div className="unsupported-network">
+        <h2>Please connect to Rinkeby</h2>
+        <p>
+          This dapp only works on the Rinkeby network, please switch networks
+          in your connected wallet.
+        </p>
+      </div>
+    );
+  }
+  
   // This is the case where the user hasn't connected their wallet
   // to your web app. Let them call connectWallet.
   if (!address) {
